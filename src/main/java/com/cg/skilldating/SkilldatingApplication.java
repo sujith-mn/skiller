4133833792c787f2c292e995366b6372e877729b
Nair <sujith.nair@capgemini.com>
2020-07-28 08:39:29 +0530

    initial commit

---
+++ src/main/java/com/cg/skilldating/SkilldatingApplication.java
@@ -0,0 +1,13 @@
+package com.cg.skilldating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilldatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilldatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/skilldating/SkilldatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.skilldating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilldatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
